2751a35916a43c7502d135aa7476436e423ab05c
Akshat Saxena <saxena.akshat.2307@gmail.com>
2025-11-08 16:41:35 +0530

    configure Reduc-ToolKit

--- 12_AppWriteMegaBlog/src/App.jsx
+++ 12_AppWriteMegaBlog/src/App.jsx
@@ -1,14 +1,40 @@
-
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
 
-function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+ import { Header, Footer } from "./components"  // Uncomment once you have these components
+import {Outlet} from 'react-router-dom'
 
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [dispatch])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-col bg-gray-400">
+      <div>Test</div>
+      <div className="w-full-block">
+        {/* <Header /> */}
+        <main>
+          <h1>Welcome to MegaBlog</h1>
+        </main>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
